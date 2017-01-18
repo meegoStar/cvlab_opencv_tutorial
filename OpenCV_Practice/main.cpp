8c6fbf088f6f94cf185215e1d9a7e1c3327870a4
meegoStar <andy19933@gmail.com>
2017-01-18 16:40:22 +0800

    Read a video from file and displayed it frame by frame

--- OpenCV_Practice/main.cpp
+++ OpenCV_Practice/main.cpp
@@ -1,12 +1,26 @@
 #include "opencv2/opencv.hpp"
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
-	Mat img = imread("penggan.jpg");
-	Mat resized_img;
-	resize(img, resized_img, Size(img.cols / 8, img.rows / 8));
-	imshow("Hello World!", resized_img);
-	waitKey();
+	Mat frame;
+	VideoCapture cap("data/jaguar.avi");
+
+	if (!cap.isOpened()) {
+		cout << "Cannot open the video" << endl;
+		return -1;
+	}
+
+	for (;;) {
+		cap.read(frame);
+
+		if (frame.rows > 0) imshow("Video Frame", frame);
+		else break;
+
+		waitKey(30);
+	}
+	return 0;
 }
